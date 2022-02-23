473775c0a9420777b2faac0626e002c948968159
azharjustin <azharjustin37@gmail.com>
2022-02-23 01:03:14 -0800

    hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const datas=[ {
+    name:"azhar" ,
+  profile:"https://a10.gaanacdn.com/gn_img/artists/a7LWBzWzXA/7LWBLgNKzX/size_xl_1631801811.webp"
+  },
+  {
+    name:"azhar" ,
+  profile:"https://a10.gaanacdn.com/gn_img/artists/a7LWBzWzXA/7LWBLgNKzX/size_xl_1631801811.webp"
+  },
+  {
+    name:"azhar" ,
+  profile:"https://a10.gaanacdn.com/gn_img/artists/a7LWBzWzXA/7LWBLgNKzX/size_xl_1631801811.webp"
+  },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {datas.map((dt)=> <Welcome  name={dt.name}profile={dt.profile}/>)}
+    <Counter/>  
     </div>
   );
 }
+function Welcome({name,profile}){
+  // const name="azhar";
+  return(
+    <div className="welcome">
+     <img className="profile" src={profile} alt="pic"/>
+      <h1 >hello {name}</h1>
+    </div>
+  );
+}
+function Counter(){
+  const [like,setLike]=useState(0);
+  return(
+    <div>
+      <button onClick={()=>{
+       setLike(like + 1);
+      }}>
+        👍 {like}
+      </button>
+      
+    </div>
+  );
 
+}
 export default App;
+
